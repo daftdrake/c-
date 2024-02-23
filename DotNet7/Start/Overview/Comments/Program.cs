7b3deb9313bc19a235e34dfddda757bc6374df67
ARosas7 <104660067+daftdrake@users.noreply.github.com>
2024-02-22 21:05:22 -0800

    first try

---
+++ DotNet7/Start/Overview/Comments/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        /// XML Comments are used to help provide documentation
+        /// They start with triple-slashes and have a special syntax
+        /// <summary>
+        /// This is the main sample application function.
+        /// </summary>
+        /// <param name='args'>An array of string arguments from the command line</param>
+        /// </returns>
+        /// No returns value
+        /// </returns>
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            //single line comments use two slashes
+            /*multiple line comments start with a slash and a star and can
+            continue for serveral lines until a 
+            closing start and a slash are encountered. */
+
+        }
+    }
+}
---
+++ DotNet7/Start/Program Flow/Conditional-switch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConditionalOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theVal = 53;
+
+            // TODO: The switch statement
+            switch (theVal) {
+                case 50:
+                    Console.WriteLine("the Value is 50.");
+                    break;
+                case 51:
+                    Console.WriteLine("theValue is 51");
+                    break;
+                case 52:
+                case 53:
+                case 54:
+                    Console.WriteLine("The value is between 52 and 54");
+                    break;
+                default:
+                    Console.WriteLine("theVal is something else.");
+                    break;
+
+            }
+
+        }
+    }
+}
